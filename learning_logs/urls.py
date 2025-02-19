bb81875ae9e5a57782752614a4a332e1269eb81f
Voyane Rogerio <voyanerogerio@hotmail.com>
2025-02-19 13:38:49 +0200

    Criado ficheiros do projecto

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -1,19 +1,4 @@
-"""
-URL configuration for learning_log project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 
 from django.urls import path  #path serve para indicar um caminho(rota) 
 from . import views
@@ -22,5 +7,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('topics', views.topics, name='topics'), #acesso a BD
-    path('topic/<topic_id>/', views.topic, name='topic'), 
+    path('topics/<topic_id>/', views.topic, name='topic'), 
+    path('new_topic', views.new_topic, name='new_topic'), 
+    path('new_entry/<topic_id>/', views.new_entry, name='new_entry'), 
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .models import Topic #importa o Topic dentro do arquivo models
+from .forms import TopicForm, EntryForm
+from django.http import HttpResponseRedirect  #so exisge que se coloque uma url
+from django.urls import reverse
 
 # Create your views here.
 
@@ -19,3 +22,36 @@ def topic(request, topic_id): #recebe dois argumentos
     entries = topic.entry_set.order_by('-date_added')  #ordenar de forma inversa.
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    """Adiciona um novo assunto."""
+    if request.method != "POST":
+        #Nenhum dado submetido. Cria um formulario em branco
+        form = TopicForm()
+    else:
+        #Dados de POST submetidos. Processa os dados
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse("topics"))
+    
+    context = {"form": form}
+    return render(request, "learning_logs/new_topic.html", context)
+
+def new_entry(request, topic_id):
+    """Adiciona uma descricao"""
+    topic = Topic.objects.get(id = topic_id)
+
+    if request.method != "POST":
+        #Nenhum dado submetido. Cria um formulario em branco
+        form = EntryForm()  
+    else: 
+        #Dados de POST submetidos. Processa os dados
+        form = EntryForm(data = request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return HttpResponseRedirect(reverse('topic', args=[topic_id]))
+    context = {'topic': topic, 'form': form}
+    return render(request, "learning_logs/new_entry.html", context)
